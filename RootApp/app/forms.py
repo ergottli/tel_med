996ee49797804c835f14ede7c7849e5c6b6a0210
noreederek <mayorovyuri7@gmail.com>
2020-06-21 07:23:35 +0300

    Services Merged by Folders

---
+++ RootApp/app/forms.py
@@ -0,0 +1,73 @@
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, \
+    TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, \
+    Length
+from app.models import User
+
+
+class LoginForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    remember_me = BooleanField('Запомнить меня')
+    submit = SubmitField('Войти')
+
+
+class RegistrationForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    email = StringField('Электронная почта', validators=[DataRequired(), Email()])
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Регистрация')
+
+    def validate_username(self, username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другое имя.')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Пожалуйста, используйте другой адрес электронной почты.')
+
+
+class ResetPasswordRequestForm(FlaskForm):
+    email = StringField('Электронная почта', validators=[DataRequired(), Email()])
+    submit = SubmitField('Запросить восстановление пароля')
+
+
+class ResetPasswordForm(FlaskForm):
+    password = PasswordField('Пароль', validators=[DataRequired()])
+    password2 = PasswordField(
+        'Повторите пароль', validators=[DataRequired(), EqualTo('password')])
+    submit = SubmitField('Запрос сброса пароля')
+
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Имя пользователя', validators=[DataRequired()])
+    about_me = TextAreaField('Обо мне', validators=[Length(min=0, max=140)])
+    submit = SubmitField('Подтвердить')
+
+    def __init__(self, original_username, *args, **kwargs):
+        super(EditProfileForm, self).__init__(*args, **kwargs)
+        self.original_username = original_username
+
+    def validate_username(self, username):
+        if username.data != self.original_username:
+            user = User.query.filter_by(username=self.username.data).first()
+            if user is not None:
+                raise ValidationError('Пожалуйста, используйте другое имя.')
+
+
+class EmptyForm(FlaskForm):
+    submit = SubmitField('Подтвердить')
+
+
+class PostForm(FlaskForm):
+<<<<<<< HEAD
+    post = TextAreaField('Создать запрос', validators=[DataRequired()])
+=======
+    post = TextAreaField('Say something', validators=[DataRequired()])
+>>>>>>> cfebcc893638e5eabc57ea920b01d2bd9a73ff66
+    submit = SubmitField('Submit')
